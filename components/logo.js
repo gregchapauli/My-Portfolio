e73f559025b108a523529d14e87088515c7d62bc
Gregory Caloin <gregchapauli@gmail.com>
2022-05-05 17:02:04 +0200

    Modification Logo

--- components/logo.js
+++ components/logo.js
@@ -38,7 +38,7 @@ const Logo = () => {
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
-            marginLeft={5}
+            style={{ marginLeft: '5px' }}
           >
             Grégory C.
           </Text>
