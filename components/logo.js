d583df0d151a30b719b3b3f3b7b4fa2c87a608d7
Gregory Caloin <gregchapauli@gmail.com>
2022-05-05 16:23:21 +0200

    Modification Logo

--- components/logo.js
+++ components/logo.js
@@ -28,11 +28,16 @@ const Logo = () => {
     <Link href="/" scroll={false}>
       <a>
         <LogoBox>
-          <Image src={footPrintImg} width={20} height={20} alt="logo" />
+          <Image
+            src={footPrintImg}
+            width={20}
+            height={20}
+            marginRight={10}
+            alt="logo"
+          />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
-            fontWeight="bold"
           >
             Grégory C.
           </Text>
