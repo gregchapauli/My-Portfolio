d24b4ac79b50021a16e0266cf3b2627a19c81169
Gregory Caloin <gregchapauli@gmail.com>
2022-05-06 14:38:26 +0200

    suppression section dans work

--- pages/works.js
+++ pages/works.js
@@ -3,7 +3,6 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
 import thumbMarvelquizz from '../public/images/works/MarvelQuizzLanding.png'
 import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.jpg'
 import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png'
@@ -21,12 +20,6 @@ const Works = () => (
       </Heading>
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
-            A Markdown note-taking app with 100+ plugins, cross-platform and
-            encrypted data sync support
-          </WorkGridItem>
-        </Section>
         <Section>
           <WorkGridItem
             id="marvelquizz"
