79616353cdc96f31b889fe55f9f293940d73a84c
Gregory Caloin <gregchapauli@gmail.com>
2022-05-17 21:02:04 +0200

    Désactivation du lien vers Posts

--- components/navbar.js
+++ components/navbar.js
@@ -74,7 +74,7 @@ const Navbar = props => {
           <LinkItem href="/works" path={path}>
             Works
           </LinkItem>
-          <LinkItem href="/posts" /*path={path}*/>Posts</LinkItem>
+          <LinkItem /*href="/posts" path={path}*/>Posts</LinkItem>
           <LinkItem
             target="_blank"
             href="https://github.com/gregchapauli/MyPortfolio"
