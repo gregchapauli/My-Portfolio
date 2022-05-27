682781c6dec1ec852aaedb5f4eaf3b70bf0cfec3
Gregory Caloin <gregchapauli@gmail.com>
2022-05-27 10:04:59 +0200

    Nettoyage du code

--- pages/works/trouveLeNombre.js
+++ pages/works/trouveLeNombre.js
@@ -1,4 +1,4 @@
-import { Container, Badge, List, ListItem, Link, Box } from '@chakra-ui/react'
+import { Container, Badge, List, ListItem, Link } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
