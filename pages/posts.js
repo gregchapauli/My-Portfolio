dacd818dbac459ebdf76365d025a7bd01a97ef13
Gregory Caloin <gregchapauli@gmail.com>
2023-01-24 22:44:03 +0100

    error correction

--- pages/posts.js
+++ pages/posts.js
@@ -3,12 +3,12 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
-import thumbFishWorkflow from '../public/images/contents/youtube-fish-workflow.jpg'
-import thumbMyDeskSetup from '../public/images/contents/youtube-my-desk-setup.jpg'
-import thumb500PaidUsers from '../public/images/contents/blog-500-paid-users.jpg'
-import thumbFinancialGoal from '../public/images/contents/blog-financial-goal.png'
-import thumbHowToPriceYourself from '../public/images/contents/blog-how-to-price-yourself.jpg'
-import thumb50xFaster from '../public/images/contents/youtube-50x-faster.jpg'
+// import thumbFishWorkflow from '../public/images/contents/youtube-fish-workflow.jpg'
+// import thumbMyDeskSetup from '../public/images/contents/youtube-my-desk-setup.jpg'
+// import thumb500PaidUsers from '../public/images/contents/blog-500-paid-users.jpg'
+// import thumbFinancialGoal from '../public/images/contents/blog-financial-goal.png'
+// import thumbHowToPriceYourself from '../public/images/contents/blog-how-to-price-yourself.jpg'
+// import thumb50xFaster from '../public/images/contents/youtube-50x-faster.jpg'
 
 const Posts = () => (
   <Layout title="Posts">
@@ -21,12 +21,12 @@ const Posts = () => (
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
           <GridItem
             title="My Fish workflow"
-            thumbnail={thumbFishWorkflow}
+            // thumbnail={thumbFishWorkflow}
             href="https://www.youtube.com/watch?v=KKxhf50FIPI"
           />
           <GridItem
             title="My desk setup (Late 2020)"
-            thumbnail={thumbMyDeskSetup}
+            // thumbnail={thumbMyDeskSetup}
             href="https://www.youtube.com/watch?v=1OFDMwDlnOE"
           />
         </SimpleGrid>
@@ -36,12 +36,12 @@ const Posts = () => (
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
           <GridItem
             title="How I’ve Attracted The First 500 Paid Users For My SaaS That Costs $5/mo"
-            thumbnail={thumb500PaidUsers}
+            // thumbnail={thumb500PaidUsers}
             href="https://blog.inkdrop.app/how-ive-attracted-the-first-500-paid-users-for-my-saas-that-costs-5-mo-7a5b94b8e820"
           />
           <GridItem
             title="I stopped setting a financial goal for my SaaS"
-            thumbnail={thumbFinancialGoal}
+            // thumbnail={thumbFinancialGoal}
             href="https://blog.inkdrop.app/i-stopped-setting-a-financial-goal-for-my-saas-a92c3db65506"
           />
         </SimpleGrid>
@@ -51,12 +51,12 @@ const Posts = () => (
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
           <GridItem
             title="How to Price Yourself as a Freelance Developer"
-            thumbnail={thumbHowToPriceYourself}
+            // thumbnail={thumbHowToPriceYourself}
             href="https://blog.inkdrop.app/how-to-price-yourself-as-a-freelance-developer-3453dfd59d91"
           />
           <GridItem
             title="I made my React Native app 50x faster"
-            thumbnail={thumb50xFaster}
+            // thumbnail={thumb50xFaster}
             href="https://www.youtube.com/watch?v=vj723NlrIQc"
           />
         </SimpleGrid>
